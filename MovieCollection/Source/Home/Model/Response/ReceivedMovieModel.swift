b7c0da4a0ccea983e0bb65a4e6747d86bcfb024a
Minny27 <dltmdals0608@naver.com>
2022-04-14 09:23:07 +0900

    [#2]Feat: 응답 모델 구현

---
+++ MovieCollection/Source/Home/Model/Response/ReceivedMovieModel.swift
@@ -0,0 +1,12 @@
+//
+//  ReceivedMovieModel.swift
+//  MovieCollection
+//
+//  Created by SeungMin on 2022/04/13.
+//
+
+import Foundation
+
+struct ReceivedMovieModel: Codable {
+    let items: [Item]?
+}
