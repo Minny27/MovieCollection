1b38a7af72593374dd1ce60626a0f13593d78d7a
Minny27 <dltmdals0608@naver.com>
2022-04-14 09:23:35 +0900

    [#2]Feat: 응답 모델을 가공해서 저장할 테이블 뷰 셀 모델 구현

---
+++ MovieCollection/Source/Home/Model/MovieTablaViewCellModel.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTablaViewCellModel.swift
+//  MovieCollection
+//
+//  Created by SeungMin on 2022/04/13.
+//
+
+import Foundation
+
+struct MovieTableViewCellModel {
+    let title: String?
+    let linkString: String?
+    let imageUrlString: String?
+    let director: String?
+    let actors: String?
+    let rating: String?
+}
