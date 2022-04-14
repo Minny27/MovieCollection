51572fa6e5aa74671728246180dfda52dc26edac
Minny27 <dltmdals0608@naver.com>
2022-04-14 20:48:43 +0900

    [#2]Feat: 입력하는 키워드에 따라 계산되는 연산 프로퍼티 urlString 구현

--- MovieCollection/Strings/Strings.swift
+++ MovieCollection/Strings/Strings.swift
@@ -6,10 +6,13 @@
 //
 
 enum Strings {
-    static let baseUrl = "https://openapi.naver.com/v1/search/movie.json?query="
-    static let keyword =  "국가".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-    static let movieCount = "&display=20"
-    static let urlString = "\(Strings.baseUrl)\(Strings.keyword)\(Strings.movieCount)"
     static let apiKey = "zSNFHRyW116cmqwwuLRJ"
     static let password = "1PxJnc3C0L"
+    static let baseUrl = "https://openapi.naver.com/v1/search/movie.json?query="
+    static let movieCount = "&display=20"
+    static var keyword =  "국가"
+    
+    static var urlString: String {
+        return (Strings.baseUrl + Strings.keyword + Strings.movieCount).encoding() ?? ""
+    }
 }
