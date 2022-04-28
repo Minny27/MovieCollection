48af85211b92893dba5cfa146224f170c9d94e9b
Minny27 <dltmdals0608@naver.com>
2022-04-28 15:51:29 +0900

    [#17]Refactor: 이니셜라이저 추가 - 이니셜라이저 코드안에 setupCell() 메서드 호출

--- MovieCollection/Source/Home/Cell/MovieTableViewCell.swift
+++ MovieCollection/Source/Home/Cell/MovieTableViewCell.swift
@@ -80,6 +80,16 @@ class MovieTableViewCell: UITableViewCell {
         return label
     }()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func setupCell() {
         contentView.addSubview(containerView)
         containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
